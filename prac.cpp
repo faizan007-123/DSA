2b94d16e61218ca24f0160c1f1560d7468146e2a
Faizan Mhate <mhatefaizan@gmail.com>
2026-01-14 20:00:13 +0530

    Add files via upload

---
+++ prac.cpp
@@ -0,0 +1,117 @@
+/* #include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{  
+    vector <int> v = {12,13,45,44,89,00};
+    v.push_back(5);
+    v.push_back(3);
+
+    for(int i : v)
+        cout << i << " ";
+
+    cout << "\n" << v.size() << " " << v.capacity() << endl; 
+    cout << v.front() << " " << v.back() << endl;
+    return 0;
+} */
+
+#include <iostream>
+using namespace std;
+int main()
+{   
+    int a[3][3];
+    int b[3][3];
+    int c[3][3];
+    int choice;
+
+    cout << "Enter elements for matrix A: ";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            cin >> a[i][j];
+    }
+    
+    cout << "\nEnter elements for matrix B: ";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            cin >> b[i][j];
+    }
+    
+    do
+    {
+        cout << "Enter your choice: \n";
+        cout << "1. Addition of A and B \n";
+        cout << "2. Subtraction of A and B \n"; 
+        cout << "3. Multiplication of A and B \n"; 
+        cout << "4. Transpose of A \n";
+        cout << "5. Exit \n"; 
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                    c[i][j] = a[i][j] + b[i][j];
+            }
+
+            cout << "Addition of A and B: \n";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    cout << c[i][j] << "  ";                
+                
+                cout << "\n";
+            }
+            break;
+     
+            case 2:
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                    c[i][j] = a[i][j] - b[i][j];
+            }
+
+            cout << "Subtraction of A and B: \n";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    cout << c[i][j] << "  ";                
+                
+                cout << "\n";
+            }
+            break;
+     
+            case 3:
+
+            break;
+
+            case 4:
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                    c[i][j] = a[j][i];
+            }
+
+            cout << "Transpose of A: \n";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    cout << c[i][j] << "  ";                
+                
+                cout << "\n";
+            }
+            break;
+
+            default:
+            cout << "Invalid Input...\n";
+            cout << "Try again...\n";
+            break;
+        }
+
+    } while (choice != 5);
+    
+    return 0;
+}
